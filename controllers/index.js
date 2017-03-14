1c60e30ae910945134c456a2354c7296e498c7c8
Unknown <p.yann138@gmail.com>
2017-03-14 21:51:55 +0100

    Delete useless file

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,6 @@ router.use('/line', require('./api-tub/ligne'))
 router.use('/user', require('./api-tub/user'))
 router.use('/stop', require('./api-tub/StopController'))
 router.use('/calculate', require('./api-tub/calculate'))
-router.use('/swfact', require('./api-starwars/swfact'))
 
 router.get('/', function(req, res) {
   res.end("Api Tub");
